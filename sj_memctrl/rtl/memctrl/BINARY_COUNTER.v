3146f81901975337e698beafe3c6903881248fca
5tmdwns <sxungz@gmail.com>
2024-12-02 09:52:45 -0500

    upload:BINARY_COUNTER.v,GRAY_COUNTER.v,LFSR16B.v,LFSR8B.v

---
+++ sj_memctrl/rtl/memctrl/BINARY_COUNTER.v
@@ -0,0 +1,15 @@
+module BINARY_COUNTER #(
+    parameter WIDTH = 16
+)(
+    input wire CLK,
+    input wire RSTN,
+    output reg [WIDTH-1:0] OUT
+);
+    always @(posedge CLK or posedge RSTN) begin
+        if (RSTN) begin
+            OUT <= 0;
+        end else begin
+            OUT <= OUT + 1;
+        end
+    end
+endmodule
